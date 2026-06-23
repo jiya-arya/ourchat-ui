9bd09e325746f3f456725f2b292403ee11c0056a
Jiya <jiyaarya1205@gmail.com>
2026-06-23 22:44:44 +0530

    added logo direction

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import './App.css';
 
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import Features from "./components/Features";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Hero />
-//       <Features />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import MessagingApp from "./components/MessagingApp";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
-  return <MessagingApp />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
+// import MessagingApp from "./components/MessagingApp";
+
+// function App() {
+//   return <MessagingApp />;
+// }
+
+// export default App;
